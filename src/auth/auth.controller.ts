1b76b2526429ef68ee7faa1b212546ff16211c8f
hobiJeong <jjb26433@naver.com>
2024-04-28 21:21:58 +0900

    feat: create token api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,53 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Headers, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Post('token/access')
+  postTokenAccess(@Headers('authorization') rawToken: string) {
+    const token = this.authService.extractTokenFormHeader(rawToken, true);
+
+    const newToken = this.authService.rotateToken(token, false);
+
+    /**
+     * {accessToken: {token}}
+     */
+    return {
+      accessToken: newToken,
+    };
+  }
+
+  @Post('token/refresh')
+  postTokenRefresh(@Headers('authorization') rawToken: string) {
+    const token = this.authService.extractTokenFormHeader(rawToken, true);
+
+    const newToken = this.authService.rotateToken(token, true);
+
+    /**
+     * {refreshToken: {token}}
+     */
+    return {
+      refreshToken: newToken,
+    };
+  }
+
   @Post('login/email')
-  loginEmail(@Body('email') email: string, @Body('password') password: string) {
+  postLoginEmail(@Headers('authorization') rawToken: string) {
+    // email:password -> base64
+    const token = this.authService.extractTokenFormHeader(rawToken, false);
+
+    const credentials = this.authService.decodeToken(token);
+
     return this.authService.loginWithEmail({
-      email,
-      password,
+      email: credentials.email,
+      password: credentials.password,
     });
   }
 
   @Post('register/email')
-  registerEmail(
+  postRegisterEmail(
     @Body('nickname') nickname: string,
     @Body('email') email: string,
     @Body('password') password: string,
