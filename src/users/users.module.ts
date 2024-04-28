4c0a61f98d94d7d660ed638c3b37906a61290ffb
hobiJeong <jjb26433@naver.com>
2024-04-28 18:33:29 +0900

    feat: 이메일 비번 검증 로직 추가

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -8,5 +8,6 @@ import { UsersModel } from 'src/users/entities/users.entity';
   imports: [TypeOrmModule.forFeature([UsersModel])],
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
