71c1c3637333b1257bfb239cd932ecea39126492
hobiJeong <jjb26433@naver.com>
2024-05-27 15:57:51 +0900

    setting: 프리티어 eslint 충돌 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,13 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    "prettier",
+    "eslint:recommended",
+    "plugin:prettier/recommended"
   ],
   root: true,
   env: {
@@ -21,5 +24,11 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    "prettier/prettier": [
+      'error',
+      {
+          endOfLine: 'auto',
+      },
+    ],  
   },
 };
