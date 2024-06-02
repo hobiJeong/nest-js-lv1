0060fba4c0398a3498b598dd17c112d914063d5f
hobiJeong <jjb26433@naver.com>
2024-06-02 22:57:48 +0900

    feat: export AuthService, PostsModule에서 Users,AuthModule import

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,12 +3,14 @@ import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostsModel } from 'src/posts/entities/posts.entity';
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
 
 /**
  * TypeORM 모델과 연동이 되는 레포지터리의 모듈을 import 해줘야 주입 가능. --> forFeature
  */
 @Module({
-  imports: [TypeOrmModule.forFeature([PostsModel])],
+  imports: [TypeOrmModule.forFeature([PostsModel]), AuthModule, UsersModule],
   controllers: [PostsController],
   providers: [PostsService],
 })
