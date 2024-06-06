2d978e4ba9bbe2965bc10bf48dd1e935b782755a
hobiJeong <jjb26433@naver.com>
2024-06-06 20:25:50 +0900

    delete: useless import

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -12,7 +12,6 @@ import {
 import { PostsService } from './posts.service';
 import { AccessTokenGuard } from 'src/auth/guard/bearer-token.guard';
 import { User } from 'src/users/decorator/user.decorator';
-import { UsersModel } from 'src/users/entities/users.entity';
 
 /**
  * author: string;
@@ -46,11 +45,11 @@ export class PostsController {
   @Post()
   @UseGuards(AccessTokenGuard)
   postPosts(
-    @User() user: UsersModel,
+    @User('id') userId: number,
     @Body('title') title: string,
     @Body('content') content: string,
   ) {
-    return this.postsService.createPost(user.id, title, content);
+    return this.postsService.createPost(userId, title, content);
   }
 
   // 4) PUT /posts/:id
