86246b61f00b7749f0360031e3839382d2fa0704
hobiJeong <jjb26433@naver.com>
2024-06-11 15:56:48 +0900

    feat: transformOptions 활성화

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,16 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // ValidationPipe를 거치고 들어온 Dto의 프로퍼티에 default값을 할당해주고 싶다면 true
+      // 그 외에도 많은 용도를 위해 쓰임
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
 
   await app.listen(3000);
 }
