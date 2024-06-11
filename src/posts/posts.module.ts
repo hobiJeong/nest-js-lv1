e5cac518c723f69061989324a2515914c4c1f5e5
hobiJeong <jjb26433@naver.com>
2024-06-11 23:53:45 +0900

    feat: 이미지 선업로드 방식으로 변경을 위해 temp 폴더 및 common controller에 엔드포인트 추가

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -22,43 +22,6 @@ import { v4 as uuid } from 'uuid';
     AuthModule,
     UsersModule,
     CommonModule,
-    MulterModule.register({
-      limits: {
-        // byte 단위로 입력
-        fileSize: 10_000_000,
-      },
-      fileFilter: (req, file, cb) => {
-        /**
-         * cb(에러, boolean)
-         *
-         * 첫번째 파라미터에는 에러가 있을 경우 에러 정보를 넣어준다.
-         * 두번째 파라미터에는 파일을 받을지 말지 boolean을 넣어준다.
-         */
-        // xxx.jpg -> .jpg
-        const ext = extname(file.originalname);
-
-        if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png') {
-          return cb(
-            new BadRequestException('jpg/jpeg/png 파일만 업로드 가능합니다.'),
-            false,
-          );
-        }
-
-        return cb(null, true);
-      },
-
-      storage: multer.diskStorage({
-        // post image의 저장 경로
-        destination: (req, res, cb) => {
-          cb(null, POST_IMAGE_PATH);
-        },
-        filename: (req, file, cb) => {
-          // 파일의 originalname이 아닌 절대로 겹치지 않을 이름으로 file을 저장
-          // 1231231-123-123123-123123.jpg
-          cb(null, `${uuid()}${extname(file.originalname)}`);
-        },
-      }),
-    }),
   ],
   controllers: [PostsController],
   providers: [PostsService],
