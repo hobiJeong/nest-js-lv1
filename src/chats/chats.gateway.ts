e755acdf7cbbf5eaeb55e17a4bc62d89c0643c53
hobiJeong <jjb26433@naver.com>
2024-06-18 16:59:07 +0900

    feat: room 개념 적용

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -24,13 +24,26 @@ export class ChatsGateway implements OnGatewayConnection {
     console.log(`on connect called : ${socket.id}`);
   }
 
+  @SubscribeMessage('enter_chat')
+  enterChat(
+    // 방의 chat ID들을 리스트로 받는다.
+    @MessageBody() data: number[],
+    @ConnectedSocket() socket: Socket,
+  ) {
+    for (const chatId of data) {
+      // socket.join()
+      socket.join(chatId.toString());
+    }
+  }
+
   // socket.on('send_message', (message) => { console.log(message) });
   @SubscribeMessage('send_message')
   sendMessage(
-    @MessageBody() message: string,
+    @MessageBody() message: { message: string; chatId: number },
     @ConnectedSocket() socket: Socket,
   ) {
-    this.server.emit('receive_message', 'hello from server');
-    socket.emit('receive_message', '특정 소켓 메세지');
+    this.server
+      .in(message.chatId.toString())
+      .emit('receive_message', message.message);
   }
 }
