860d92fde0a367d9e33012db84b8f8be02fe209b
hobiJeong <jjb26433@naver.com>
2024-06-18 17:33:06 +0900

    feat: paginationChats API 구현

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -4,9 +4,10 @@ import { ChatsController } from './chats.controller';
 import { ChatsGateway } from 'src/chats/chats.gateway';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChatsModel } from 'src/chats/entity/chats.entity';
+import { CommonModule } from 'src/common/common.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ChatsModel])],
+  imports: [TypeOrmModule.forFeature([ChatsModel]), CommonModule],
   controllers: [ChatsController],
   providers: [ChatsGateway, ChatsService],
 })
--- src/chats/chats.service.ts
+++ src/chats/chats.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateChatDto } from 'src/chats/dto/create-chat.dto';
+import { PaginateChatDto } from 'src/chats/dto/paginate-chat.dto';
 import { ChatsModel } from 'src/chats/entity/chats.entity';
+import { CommonService } from 'src/common/common.service';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -9,7 +11,17 @@ export class ChatsService {
   constructor(
     @InjectRepository(ChatsModel)
     private readonly chatsRepository: Repository<ChatsModel>,
+    private readonly commonService: CommonService,
   ) {}
+
+  paginateChats(dto: PaginateChatDto) {
+    return this.commonService.paginate(
+      dto,
+      this.chatsRepository,
+      { relations: { users: true } },
+      'chats',
+    );
+  }
 
   async createChat(dto: CreateChatDto) {
     const chat = await this.chatsRepository.save({
