7d5bce9a696a2d75f3979c28e5333ff49d7f31c3
hobiJeong <jjb26433@naver.com>
2024-06-19 19:44:55 +0900

    feat: comments Patch, Delete API 생성

--- src/posts/comments/comments.controller.ts
+++ src/posts/comments/comments.controller.ts
@@ -1,9 +1,11 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   Param,
   ParseIntPipe,
+  Patch,
   Post,
   Query,
   UseGuards,
@@ -14,6 +16,7 @@ import { AccessTokenGuard } from 'src/auth/guard/bearer-token.guard';
 import { User } from 'src/users/decorator/user.decorator';
 import { UsersModel } from 'src/users/entity/users.entity';
 import { CreateCommentDto } from 'src/posts/comments/dto/create-comment.dto';
+import { UpdateCommentDto } from 'src/posts/comments/dto/update-comment.dto';
 
 @Controller('posts/:postId/comments')
 export class CommentsController {
@@ -40,5 +43,20 @@ export class CommentsController {
     @User() user: UsersModel,
   ) {
     return this.commentsService.createComment(body, postId, user);
+  }
+
+  @Patch(':commentId')
+  @UseGuards(AccessTokenGuard)
+  async patchComment(
+    @Param('commentId', ParseIntPipe) commentId: number,
+    @Body() body: UpdateCommentDto,
+  ) {
+    return this.commentsService.updateComment(body, commentId);
+  }
+
+  @Delete(':commentId')
+  @UseGuards(AccessTokenGuard)
+  async deleteComment(@Param('commentId', ParseIntPipe) commentId: number) {
+    return this.commentsService.deleteComment(commentId);
   }
 }
--- src/posts/comments/comments.service.ts
+++ src/posts/comments/comments.service.ts
@@ -4,6 +4,7 @@ import { CommonService } from 'src/common/common.service';
 import { DEFAULT_COMMENT_FIND_OPTIONS } from 'src/posts/comments/const/default-comment-find-options.const';
 import { CreateCommentDto } from 'src/posts/comments/dto/create-comment.dto';
 import { PaginateCommentsDto } from 'src/posts/comments/dto/paginate-comments.dto';
+import { UpdateCommentDto } from 'src/posts/comments/dto/update-comment.dto';
 import { CommentsModel } from 'src/posts/comments/entity/comments.entity';
 import { UsersModel } from 'src/users/entity/users.entity';
 import { Repository } from 'typeorm';
@@ -61,5 +62,42 @@ export class CommentsService {
       author,
       likeCount: 0,
     });
+  }
+
+  async updateComment(dto: UpdateCommentDto, commentId: number) {
+    const comment = await this.commentsRepository.findOne({
+      where: {
+        id: commentId,
+      },
+    });
+
+    if (!comment) {
+      throw new BadRequestException(`존재하지 않는 댓글입니다.`);
+    }
+
+    const prevComment = await this.commentsRepository.preload({
+      id: commentId,
+      ...dto,
+    });
+
+    const newComment = await this.commentsRepository.save(prevComment);
+
+    return newComment;
+  }
+
+  async deleteComment(id: number) {
+    const comment = await this.commentsRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!comment) {
+      throw new BadRequestException(`존재하지 않는 댓글입니다.`);
+    }
+
+    await this.commentsRepository.delete(id);
+
+    return id;
   }
 }
