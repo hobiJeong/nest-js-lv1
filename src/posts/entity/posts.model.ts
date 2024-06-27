1e5ed1c5ea9c0c1e537c57b4a99a118d07dc016d
hobiJeong <jjb26433@naver.com>
2024-06-27 14:25:11 +0900

    feat: PostsModel 추가

---
+++ src/posts/entity/posts.model.ts
@@ -0,0 +1,10 @@
+export class PostsModel {
+  private _id: number;
+  private _title: string;
+  private _content: string;
+  private _likeCount: number;
+  private _commentCount: number;
+  private _authorId: number;
+  private _createdAt: Date;
+  private _updatedAt: Date;
+}
