4c4ca59489343434140db7f8db4c2294fb65b382
Jeongbiho <bhjeong@missgoauction.com>
2024-08-14 15:21:02 +0900

    feat: post repository DI 토큰 생성 및 주입

--- src/apis/posts/commands/create-post/create-post.handler.ts
+++ src/apis/posts/commands/create-post/create-post.handler.ts
@@ -1,15 +1,18 @@
 import { Inject } from '@nestjs/common';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { CreatePostCommand } from '@src/apis/posts/commands/create-post/create-post.command';
+import { POST_REPOSITORY_TOKEN } from '@src/apis/posts/tokens/di.token';
 
 @CommandHandler(CreatePostCommand)
 export class CreatePostHandler implements ICommandHandler<CreatePostCommand> {
     constructor(
-        @Inject()
+        @Inject(POST_REPOSITORY_TOKEN) postsRepository: PostsRepositoryPort
     ) {}
 
     execute(command: CreatePostCommand): Promise<any> {
-        
+        const { title, content, userId, images } = command;
+
+        const entity = UserEntity.create();
     }
 }
 
