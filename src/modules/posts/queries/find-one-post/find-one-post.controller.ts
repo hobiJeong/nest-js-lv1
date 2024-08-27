568ca95cbc68b2e378db1a09f777967fcfb9481f
hobiJeong <jjb26433@naver.com>
2024-08-28 01:14:53 +0900

    modify: FindOneQueryBase 삭제

--- src/modules/posts/queries/find-one-post/find-one-post.controller.ts
+++ src/modules/posts/queries/find-one-post/find-one-post.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { routesV1 } from '@config/app.route';
+import { FindOnePostQuery } from '@modules/posts/queries/find-one-post/find-one-post.query';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { QueryBus } from '@nestjs/cqrs';
 
-@Controller()
+@Controller(routesV1.version)
 export class FindOnePostController {
-    @Get()
+  constructor(private readonly queryBus: QueryBus) {}
+
+  @Get(routesV1.post.findOne)
+  async findOne(@Param('id', ParseIntPipe) id: bigint) {
+    const query = new FindOnePostQuery({ id });
+
+    const post = await this.queryBus.execute(query);
+  }
 }
--- src/modules/posts/queries/find-one-post/find-one-post.query.ts
+++ src/modules/posts/queries/find-one-post/find-one-post.query.ts
@@ -1,8 +1,18 @@
-import { FindOneQuery } from '@libs/ddd/find-one-query.base';
-import { IQuery } from '@nestjs/cqrs';
+import { IQuery, IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 
-export class FindOnePostQuery extends FindOneQuery implements IQuery {
+export class FindOnePostQuery implements IQuery {
+  readonly id: bigint;
+
   constructor(props: FindOnePostQuery) {
-    super(props);
+    const { id } = props;
+
+    this.id = id;
   }
+}
+
+@QueryHandler(FindOnePostQuery)
+export class FindOnePostQueryHandler
+  implements IQueryHandler<FindOnePostQuery>
+{
+  execute(query: FindOnePostQuery): Promise<any> {}
 }
